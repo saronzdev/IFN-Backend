347287ed4c619d92d101a57dcdcc1d8845027a66
saronzdev <ronaldsrd@protonmail.com>
2025-06-05 00:54:48 -0400

    fix cors issues

--- index.js
+++ index.js
@@ -32,11 +32,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(cors({
-  origin: ['http://localhost:4321', DOMAIN],
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type']
-}))
+app.use(cors())
 
 app.use(express.json())
 
